8cd325b2af08c583561b770649087715de9751a2
Semolik <70706334+Semolik@users.noreply.github.com>
2024-02-23 13:31:34 +0700

    Update websockets.py

--- app/utilities/websockets.py
+++ app/utilities/websockets.py
@@ -1,11 +1,11 @@
 from fastapi import Depends, WebSocket, WebSocketException
-from db.db import get_async_session
-from users_controller import verify_token
+from users_controller import get_jwt_strategy
+from users_controller import get_jwt_strategy, get_user_manager
 
 
-async def get_user_from_cookie(websocket: WebSocket, db=Depends(get_async_session)):
+async def get_user_from_cookie(websocket: WebSocket, user_manager=Depends(get_user_manager), is_verified: bool = False):
     cookie = websocket.cookies.get("fastapiusersauth")
-    user = await verify_token(token=cookie, db=db)
-    if not user or not user.is_active:
-        raise WebSocketException(code=401, reason="Unauthorized")
+    user = await get_jwt_strategy().read_token(cookie, user_manager)
+    if not user or not user.is_active or user.is_verified if is_verified else False:
+        raise WebSocketException("Invalid user")
     return user
