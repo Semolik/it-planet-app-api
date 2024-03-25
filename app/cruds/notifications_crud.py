8d51967962fc890428934cb06c53dccedb2311e8
Semolik <70706334+Semolik@users.noreply.github.com>
2024-03-25 18:07:54 +0700

    added ws and read all endpoints to notifications

--- app/cruds/notifications_crud.py
+++ app/cruds/notifications_crud.py
@@ -1,10 +1,11 @@
+from datetime import datetime
 import uuid
 from models.notifications import Notification
 from cruds.base_crud import BaseCRUD
 from models.locations import Institution
 from sqlalchemy.future import select
 from sqlalchemy.orm import selectinload
-from sqlalchemy import func, select
+from sqlalchemy import func, select, update
 
 
 class NotificationsCrud(BaseCRUD):
@@ -26,11 +27,26 @@ class NotificationsCrud(BaseCRUD):
             )
         )
 
-    async def get_notifications(self, user_id: uuid.UUID, is_read: bool = None) -> list[Notification]:
+    async def read_all_notifications(self, user_id: uuid.UUID) -> None:
+        await self.db.execute(
+            update(Notification).where(
+                Notification.user_id == user_id,
+                Notification.read == False
+            ).values(
+                read=True
+            )
+        )
+        await self.db.commit()
+
+    async def get_notifications(self, user_id: uuid.UUID, is_read: bool = None, page: int = 1, page_size: int = 20) -> list[Notification]:
         query = select(Notification).where(Notification.user_id == user_id)
         if is_read is not None:
             query = query.where(Notification.read == is_read)
-        result = await self.db.execute(query.order_by(Notification.creation_date.desc()))
+        query = query.order_by(Notification.creation_date.desc())
+        end = page * page_size
+        start = end - page_size
+
+        result = await self.db.execute(query.slice(start, end))
         return result.scalars().all()
 
     async def get_notifications_count(self, user_id: uuid.UUID, is_read: bool = None) -> int:
--- app/endpoints/notifications.py
+++ app/endpoints/notifications.py
@@ -1,6 +1,8 @@
 from typing import List
 import uuid
-from fastapi import APIRouter, Depends, HTTPException, Path
+from fastapi import APIRouter, Depends, HTTPException, Path, Query, WebSocket, WebSocketDisconnect, WebSocketException
+from notifier import Notifier, get_notifier
+from utilities.websockets import get_user_from_cookie
 from users_controller import current_active_user
 from db.db import get_async_session
 from cruds.notifications_crud import NotificationsCrud
@@ -10,8 +12,24 @@ api_router = APIRouter(prefix="/notifications", tags=["notifications"])
 
 
 @api_router.get("", response_model=List[Notification])
-async def get_notifications(db=Depends(get_async_session), current_user=Depends(current_active_user)):
-    return await NotificationsCrud(db).get_notifications(current_user.id)
+async def get_notifications(page: int = Query(ge=1), db=Depends(get_async_session), current_user=Depends(current_active_user)):
+    return await NotificationsCrud(db).get_notifications(current_user.id, page=page)
+
+
+@api_router.websocket("/ws")
+async def websocket_endpoint(
+    websocket: WebSocket,
+    current_user=Depends(get_user_from_cookie)
+):
+    if not current_user:
+        raise WebSocketException(code=403, reason="Access denied")
+    notifier: Notifier = get_notifier(f'notifications')()
+    try:
+        await notifier.connect(user_id=current_user.id, websocket=websocket)
+        while True:
+            await websocket.receive()
+    except WebSocketDisconnect:
+        notifier.remove(user_id=current_user.id, websocket=websocket)
 
 
 @api_router.get("/unread", response_model=List[Notification])
@@ -24,8 +42,16 @@ async def get_unread_notifications_count(db=Depends(get_async_session), current_
     return await NotificationsCrud(db).get_notifications_count(current_user.id, is_read=False)
 
 
-@api_router.post('/{notification_id}/read', status_code=204)
-async def read_notification(notification_id: uuid.UUID = Path(..., description='ID уведомления'),
+@api_router.post("/read", status_code=204)
+async def read_notification(
+    db=Depends(get_async_session),
+    current_user=Depends(current_active_user)
+):
+    await NotificationsCrud(db).read_all_notifications(user_id=current_user.id)
+
+
+@api_router.post("/{notification_id}/read", status_code=204)
+async def read_notification(notification_id: uuid.UUID = Path(..., description="ID уведомления"),
                             db=Depends(get_async_session), current_user=Depends(current_active_user)):
     notification = await NotificationsCrud(db).get_notification(notification_id)
     if not notification or notification.user_id != current_user.id:
@@ -33,16 +59,16 @@ async def read_notification(notification_id: uuid.UUID = Path(..., description='
     await NotificationsCrud(db).read_notification(notification)
 
 
-@api_router.get('/{notification_id}', response_model=Notification)
-async def get_notification(notification_id: uuid.UUID = Path(..., description='ID уведомления'), db=Depends(get_async_session), current_user=Depends(current_active_user)):
+@api_router.get("/{notification_id}", response_model=Notification)
+async def get_notification(notification_id: uuid.UUID = Path(..., description="ID уведомления"), db=Depends(get_async_session), current_user=Depends(current_active_user)):
     notification = await NotificationsCrud(db).get_notification(notification_id)
     if not notification or notification.user_id != current_user.id:
         raise HTTPException(404, "Уведомление не найдено")
     return notification
 
 
-@api_router.delete('/{notification_id}', status_code=204)
-async def delete_notification(notification_id: uuid.UUID = Path(..., description='ID уведомления'), db=Depends(get_async_session), current_user=Depends(current_active_user)):
+@api_router.delete("/{notification_id}", status_code=204)
+async def delete_notification(notification_id: uuid.UUID = Path(..., description="ID уведомления"), db=Depends(get_async_session), current_user=Depends(current_active_user)):
     notification = await NotificationsCrud(db).get_notification(notification_id)
     if not notification or notification.user_id != current_user.id:
         raise HTTPException(404, "Уведомление не найдено")
