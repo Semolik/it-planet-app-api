075a1733e486675f44c641d9508e86b90df92eec
Semolik <70706334+Semolik@users.noreply.github.com>
2024-04-08 15:46:43 +0700

    cascade delete likes

--- app/models/user.py
+++ app/models/user.py
@@ -38,8 +38,8 @@ class UserLike(Base):
     __tablename__ = "user_likes"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    user_id = Column(UUID(as_uuid=True), ForeignKey(
-        'users.id'), nullable=False)
+    user_id = Column(UUID(as_uuid=True), ForeignKey('users.id',ondelete='CASCADE'
+        ), nullable=False)
     user = relationship(User, foreign_keys=[user_id])
     liked_user_id = Column(
         UUID(as_uuid=True), ForeignKey('users.id'), nullable=False)
